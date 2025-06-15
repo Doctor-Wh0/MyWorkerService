a172b8cc9587bfc2f75848d933ec0b0ec16f6c08
Maxim Gordeev <maksim.gordeew2012@yandex.ru>
2025-06-15 15:44:12 +0400

    fixes

--- Clients/MoexClient.cs
+++ Clients/MoexClient.cs
@@ -2,35 +2,26 @@ namespace MyWorkerService.Clients;
 
 public class MoexClient : IApiClient
 {
-    ///private readonly HttpClient _httpClient;
+    private readonly HttpClient _httpClient;
+    //private static string BaseUri => "http://iss.moex.com";
 
-    ///public MoexClient(HttpClient httpClient)
-    ///{
-    ///    _httpClient = httpClient;
-    ///}
-
-    private static string Uri => "http://iss.moex.com";
-    private static string url = "http://iss.moex.com/iss/history/engines/stock/markets/shares/securities/{shareTickerProperty}.{dataFormatProperty}?from={fromProperty}&till={tillProperty}&interval={intervalProperty}";
-
+    public MoexClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        //_httpClient.BaseAddress = new Uri(BaseUri); // Установка базового адреса, так как это может быть нужно для всех запросов
+    }
 
     public async Task<List<TradeRecord>> FetchDataAsync(string url)
     {
-        using (var httpClient = new HttpClient())
-        {
-            // Указываем базовый адрес API MOEX
-            httpClient.BaseAddress = new Uri(Uri);
+        // Выполняем GET-запрос
+        var response = await _httpClient.GetAsync(url);
 
-            // Выполняем GET-запрос
-            var response = await httpClient.GetAsync(url);
+        // Проверяем успешность ответа
+        response.EnsureSuccessStatusCode();
 
-            // Проверяем успешность ответа
-            response.EnsureSuccessStatusCode();
-
-
-            // Читаем и возвращаем содержимое ответа как строку
-            var res = await response.Content.ReadAsStringAsync();
-            return TradeRecord.Deserialize(res);
-        }
+        // Читаем и возвращаем содержимое ответа как строку
+        var res = await response.Content.ReadAsStringAsync();
+        return TradeRecord.Deserialize(res);
     }
 
     public async Task<List<TradeRecord>> FetchSharesAsync(string ticker, string period)
@@ -43,14 +34,12 @@ public class MoexClient : IApiClient
             "1w" => DateTime.Today.AddDays(-7).ToString("yyyy-MM-dd"),
             "3d" => DateTime.Today.AddDays(-3).ToString("yyyy-MM-dd"),
             "1d" => DateTime.Today.AddDays(-1).ToString("yyyy-MM-dd"),
-            _ => throw new ArgumentException()
+            _ => throw new ArgumentException("Invalid period")
         };
 
-        string url = $"iss/history/engines/stock/markets/shares/securities/{ticker}.json?from={lastRecordData}&till={DateTime.Now.ToString("yyyy-MM-dd")}&interval={31}";
+        string url = $"iss/history/engines/stock/markets/shares/securities/{ticker}.json?from={lastRecordData}&till={DateTime.Now:yyyy-MM-dd}&interval={31}";
         var task = await FetchDataAsync(url);
 
         return task;
     }
-
-
 }
